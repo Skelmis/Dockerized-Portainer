41311f2e37340d5b67b9f065472f3877af3c4b93
Skelmis <47520067+Skelmis@users.noreply.github.com>
2022-02-08 00:51:23 +1300

    Update docker-entrypoint.sh

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -5,6 +5,5 @@ touch /var/log/gunicorn.log
 touch /var/log/gunicorn-access.log
 tail -n 0 -f /var/log/gunicorn*.log &
 
-python manage.py makemigrations
 python manage.py migrate
 exec gunicorn (named dir).wsgi:application -c gunicorn.conf.py
